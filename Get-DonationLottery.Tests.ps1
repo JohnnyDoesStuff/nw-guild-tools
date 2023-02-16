810ec3a044dd6186261a52f733bbbff69ddeca18
JohnnyDoesStuff <johnny.doesstuff@freenet.de>
2023-02-16 18:48:18 +0100

    make Get-DonationLottery use an input object instead of a csv file

--- Get-DonationLottery.Tests.ps1
+++ Get-DonationLottery.Tests.ps1
@@ -5,75 +5,51 @@ Describe "Test Get-DonationLottery" {
     }
     Context "Invalid data" {
         It "Not enough people" {
-            Mock Import-Csv -ParameterFilter {
-                $Path -eq $fakePath
-            } -MockWith {
-                @(
-                    @{Name = "foo0"; Points = 10}
-                    @{Name = "foo1"; Points = 10}
-                    @{Name = "foo2"; Points = 10}
-                )
-            } -Verifiable
+            $pointsPerAccount = @(
+                @{Name = "foo0"; Points = 10}
+                @{Name = "foo1"; Points = 10}
+                @{Name = "foo2"; Points = 10}
+            )
 
-            { Get-DonationLottery -CsvWithPeople $fakePath -ListLength 5 -PointThreshold 9} | Should -Throw
-
-            Should -InvokeVerifiable
+            { Get-DonationLottery -PointsPerAccount $pointsPerAccount -ListLength 5 -PointThreshold 9} | Should -Throw
         }
 
         It "No participants" {
-            Mock Import-Csv -ParameterFilter {
-                $Path -eq $fakePath
-            } -MockWith {
-                @(
-                    @{Name = "foo0"; Points = 0}
-                    @{Name = "foo1"; Points = 0}
-                    @{Name = "foo2"; Points = 0}
-                    @{Name = "foo3"; Points = 0}
-                    @{Name = "foo4"; Points = 0}
-                    @{Name = "foo5"; Points = 0}
-                )
-            } -Verifiable
+            $pointsPerAccount =@(
+                @{Name = "foo0"; Points = 0}
+                @{Name = "foo1"; Points = 0}
+                @{Name = "foo2"; Points = 0}
+                @{Name = "foo3"; Points = 0}
+                @{Name = "foo4"; Points = 0}
+                @{Name = "foo5"; Points = 0}
+            )
 
-            { Get-DonationLottery -CsvWithPeople $fakePath -ListLength 5 -PointThreshold 9} | Should -Throw
-
-            Should -InvokeVerifiable
+            { Get-DonationLottery -PointsPerAccount $pointsPerAccount -ListLength 5 -PointThreshold 9} | Should -Throw
         }
 
         It "No people" {
-            Mock Import-Csv -ParameterFilter {
-                $Path -eq $fakePath
-            } -MockWith {
-                @()
-            } -Verifiable
+            $pointsPerAccount = @()
 
-            { Get-DonationLottery -CsvWithPeople $fakePath -ListLength 5 -PointThreshold 9} | Should -Throw
-
-            Should -InvokeVerifiable
+            { Get-DonationLottery -PointsPerAccount $pointsPerAccount -ListLength 5 -PointThreshold 9} | Should -Throw
         }
 
         It "Not enough qualified people" {
-            Mock Import-Csv -ParameterFilter {
-                $Path -eq $fakePath
-            } -MockWith {
-                @(
-                    @{Name = "foo0"; Points = 10}
-                    @{Name = "foo1"; Points = 10}
-                    @{Name = "foo2"; Points = 10}
-                    @{Name = "foo3"; Points = 10}
-                    @{Name = "foo4"; Points = 0}
-                    @{Name = "foo5"; Points = 0}
-                )
-            } -Verifiable
+            $pointsPerAccount = @(
+                @{Name = "foo0"; Points = 10}
+                @{Name = "foo1"; Points = 10}
+                @{Name = "foo2"; Points = 10}
+                @{Name = "foo3"; Points = 10}
+                @{Name = "foo4"; Points = 0}
+                @{Name = "foo5"; Points = 0}
+            )
 
-            { Get-DonationLottery -CsvWithPeople $fakePath -ListLength 5 -PointThreshold 9} | Should -Throw
-
-            Should -InvokeVerifiable
+            { Get-DonationLottery -PointsPerAccount $pointsPerAccount -ListLength 5 -PointThreshold 9} | Should -Throw
         }
     }
 
     Context "Valid data" {
         It "Right amount of people" {
-            $mockItems = @(
+            $pointsPerAccount = @(
                 @{Name = "foo0"; Points = 10}
                 @{Name = "foo1"; Points = 10}
                 @{Name = "foo2"; Points = 10}
@@ -81,27 +57,19 @@ Describe "Test Get-DonationLottery" {
                 @{Name = "foo4"; Points = 10}
             )
 
-            Mock Import-Csv -ParameterFilter {
-                $Path -eq $fakePath
-            } -MockWith {
-                $mockItems
-            } -Verifiable
-
-            $result = Get-DonationLottery -CsvWithPeople $fakePath -ListLength 5 -PointThreshold 9
+            $result = Get-DonationLottery -PointsPerAccount $pointsPerAccount -ListLength 5 -PointThreshold 9
 
             $result.Length | Should -Be 5
-            $result | Should -Contain $mockItems[0].Name
-            $result | Should -Contain $mockItems[1].Name
-            $result | Should -Contain $mockItems[2].Name
-            $result | Should -Contain $mockItems[3].Name
-            $result | Should -Contain $mockItems[4].Name
-
-            Should -InvokeVerifiable
+            $result | Should -Contain $pointsPerAccount[0].Name
+            $result | Should -Contain $pointsPerAccount[1].Name
+            $result | Should -Contain $pointsPerAccount[2].Name
+            $result | Should -Contain $pointsPerAccount[3].Name
+            $result | Should -Contain $pointsPerAccount[4].Name
         }
 
         It "Right people with exact threshold" {
             $threshold = 9
-            $mockItems = @(
+            $pointsPerAccount = @(
                 @{Name = "foo0"; Points = $threshold}
                 @{Name = "foo1"; Points = $threshold}
                 @{Name = "foo2"; Points = $threshold}
@@ -109,26 +77,18 @@ Describe "Test Get-DonationLottery" {
                 @{Name = "foo4"; Points = $threshold}
             )
 
-            Mock Import-Csv -ParameterFilter {
-                $Path -eq $fakePath
-            } -MockWith {
-                $mockItems
-            } -Verifiable
-
-            $result = Get-DonationLottery -CsvWithPeople $fakePath -ListLength 5 -PointThreshold $threshold
+            $result = Get-DonationLottery -PointsPerAccount $pointsPerAccount -ListLength 5 -PointThreshold $threshold
 
             $result.Length | Should -Be 5
-            $result | Should -Contain $mockItems[0].Name
-            $result | Should -Contain $mockItems[1].Name
-            $result | Should -Contain $mockItems[2].Name
-            $result | Should -Contain $mockItems[3].Name
-            $result | Should -Contain $mockItems[4].Name
-
-            Should -InvokeVerifiable
+            $result | Should -Contain $pointsPerAccount[0].Name
+            $result | Should -Contain $pointsPerAccount[1].Name
+            $result | Should -Contain $pointsPerAccount[2].Name
+            $result | Should -Contain $pointsPerAccount[3].Name
+            $result | Should -Contain $pointsPerAccount[4].Name
         }
 
         It "More people than required" {
-            $mockItems = @(
+            $pointsPerAccount = @(
                 @{Name = "foo0"; Points = 10}
                 @{Name = "foo1"; Points = 10}
                 @{Name = "foo2"; Points = 10}
@@ -138,24 +98,16 @@ Describe "Test Get-DonationLottery" {
                 @{Name = "foo6"; Points = 10}
             )
 
-            Mock Import-Csv -ParameterFilter {
-                $Path -eq $fakePath
-            } -MockWith {
-                $mockItems
-            } -Verifiable
-
-            $result = Get-DonationLottery -CsvWithPeople $fakePath -ListLength 5 -PointThreshold 9
+            $result = Get-DonationLottery -PointsPerAccount $pointsPerAccount -ListLength 5 -PointThreshold 9
 
             $result.Length | Should -Be 5
             $result | ForEach-Object {
-                $mockItems.Name | Should -Contain $_
+                $pointsPerAccount.Name | Should -Contain $_
             }
-
-            Should -InvokeVerifiable
         }
 
         It "Only right amount of people meets requirements" {
-            $mockItems = @(
+            $pointsPerAccount = @(
                 @{Name = "foo0"; Points = 10}
                 @{Name = "foo1"; Points = 10}
                 @{Name = "foo2"; Points = 10}
@@ -166,26 +118,18 @@ Describe "Test Get-DonationLottery" {
                 @{Name = "foo7"; Points = 0}
             )
 
-            Mock Import-Csv -ParameterFilter {
-                $Path -eq $fakePath
-            } -MockWith {
-                $mockItems
-            } -Verifiable
-
-            $result = Get-DonationLottery -CsvWithPeople $fakePath -ListLength 5 -PointThreshold 9
+            $result = Get-DonationLottery -PointsPerAccount $pointsPerAccount -ListLength 5 -PointThreshold 9
 
             $result.Length | Should -Be 5
-            $result | Should -Contain $mockItems[0].Name
-            $result | Should -Contain $mockItems[1].Name
-            $result | Should -Contain $mockItems[2].Name
-            $result | Should -Contain $mockItems[4].Name
-            $result | Should -Contain $mockItems[6].Name
-
-            Should -InvokeVerifiable
+            $result | Should -Contain $pointsPerAccount[0].Name
+            $result | Should -Contain $pointsPerAccount[1].Name
+            $result | Should -Contain $pointsPerAccount[2].Name
+            $result | Should -Contain $pointsPerAccount[4].Name
+            $result | Should -Contain $pointsPerAccount[6].Name
         }
 
         It "More people meet requirement and some don't" {
-            $mockItems = @(
+            $pointsPerAccount = @(
                 @{Name = "foo0"; Points = 10}
                 @{Name = "foo1"; Points = 0}
                 @{Name = "foo2"; Points = 10}
@@ -199,24 +143,16 @@ Describe "Test Get-DonationLottery" {
                 @{Name = "foo10"; Points = 10}
             )
 
-            Mock Import-Csv -ParameterFilter {
-                $Path -eq $fakePath
-            } -MockWith {
-                $mockItems
-            } -Verifiable
-
-            $result = Get-DonationLottery -CsvWithPeople $fakePath -ListLength 5 -PointThreshold 9
+            $result = Get-DonationLottery -PointsPerAccount $pointsPerAccount -ListLength 5 -PointThreshold 9
 
             $result.Length | Should -Be 5
-            $result | Should -Not -Contain $mockItems[1].Name
-            $result | Should -Not -Contain $mockItems[4].Name
-            $result | Should -Not -Contain $mockItems[7].Name
-            $result | Should -Not -Contain $mockItems[9].Name
+            $result | Should -Not -Contain $pointsPerAccount[1].Name
+            $result | Should -Not -Contain $pointsPerAccount[4].Name
+            $result | Should -Not -Contain $pointsPerAccount[7].Name
+            $result | Should -Not -Contain $pointsPerAccount[9].Name
             $result | ForEach-Object {
-                $mockItems.Name | Should -Contain $_
+                $pointsPerAccount.Name | Should -Contain $_
             }
-
-            Should -InvokeVerifiable
         }
     }
 }
--- Get-DonationLottery.ps1
+++ Get-DonationLottery.ps1
@@ -1,10 +1,33 @@
+<#
+.SYNOPSIS
+    Creates a donation lottery for multiple accounts that could achieve points
+
+.PARAMETER PointsPerAccount
+    An array with the points per accoung
+    The elements of the array must have a field 'Name' and a field 'Points'
+
+.PARAMETER ListLength
+    How many people should win something
+
+.PARAMETER PointThreshold
+    The amount of points that have to be achieved to have a chance of winning something
+
+.EXAMPLE
+    Get-DonationLottery -PointsPerAccount @(
+                    @{Name = "foo0"; Points = 10}
+                    @{Name = "foo1"; Points = 10}
+                    @{Name = "foo2"; Points = 10}
+                ) -ListLength 2 -PointThreshold 10
+#>
+
+
 
 function Get-DonationLottery {
     [CmdletBinding()]
     param (
         [Parameter(Mandatory)]
-        [String]
-        $CsvWithPeople,
+        [array]
+        $PointsPerAccount,
         [Parameter(Mandatory)]
         [int]
         $ListLength,
@@ -13,9 +36,7 @@ function Get-DonationLottery {
         $PointThreshold
     )
 
-    $allPeople = Import-Csv -Path $CsvWithPeople
-
-    if ($allPeople.Length -lt $ListLength) {
+    if ($PointsPerAccount.Length -lt $ListLength) {
         $errorMessage = @(
             "There are not enough people to create a list of the",
             "given length of $ListLength -- even without",
@@ -24,7 +45,7 @@ function Get-DonationLottery {
         throw $errorMessage
     }
 
-    $qualifiedPeople = $allPeople | Where-Object {
+    $qualifiedPeople = $PointsPerAccount | Where-Object {
         [int]$_.Points -ge $PointThreshold
     }
 
