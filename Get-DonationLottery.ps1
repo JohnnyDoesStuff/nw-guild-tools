0865223f4eb23947b80dd46609070b5779589196
JohnnyDoesStuff <johnny.doesstuff@freenet.de>
2023-02-20 17:25:47 +0100

    fix usage of Get-Content
    also makes a testcase obsolete

--- Get-DonationLottery.ps1
+++ Get-DonationLottery.ps1
@@ -55,12 +55,15 @@ function Get-DonationLottery {
     $accountsToIgnore = @()
     if (-not ([String]::IsNullOrEmpty($AccountIgnoreFile))) {
         if (Test-Path $AccountIgnoreFile) {
-            [string]$rawAccountData = Get-Content -Path $AccountIgnoreFile
-            [array]$lines = $rawAccountData.Split("`r`n")
+            [array]$lines = Get-Content -Path $AccountIgnoreFile
             [array]$accountsToIgnore = $lines | Where-Object {
                 -not [String]::IsNullOrWhiteSpace($_)
             } | ForEach-Object {
                 $_.Trim()
+            }
+            Write-Verbose "Ignoring the following accounts"
+            $accountsToIgnore | ForEach-Object {
+                Write-Verbose "    $_"
             }
         } else {
             throw "The ignore file '$AccountIgnoreFile' could not be found"
