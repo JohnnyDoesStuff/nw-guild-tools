3e222921e5412b93b05ae81316d23cea06f13972
JohnnyDoesStuff <johnny.doesstuff@freenet.de>
2023-03-23 07:21:19 +0100

    add function to handle alt accounts

---
+++ Convert-AltAccount.Tests.ps1
@@ -0,0 +1,135 @@
+Describe "Convert-AltAccount" {
+    BeforeEach {
+        . $PSScriptRoot\Convert-AltAccount.ps1
+        $donationFakePath = "fake/path/data.csv"
+        $mappingFakePath = "fake/path/mapping.txt"
+        $targetFakePath = "fake/path/result.csv"
+        Mock Get-Content -ParameterFilter {
+            ($Path -eq $donationFakePath) -and ($Raw -eq $true)
+        } -MockWith {
+            $script:datafile
+        } -Verifiable
+        Mock Get-Content -ParameterFilter {
+            ($Path -eq $mappingFakePath) -and ($Raw -ne $true)
+        } -MockWith {
+            $script:mappingFile
+        } -Verifiable
+        Mock New-Item -ParameterFilter {
+            @(
+                $Path -eq $targetFakePath
+                $ItemType -eq "File"
+                $Value -eq $script:expectedResult
+            ) -notcontains $false
+        } -MockWith {} -Verifiable
+    }
+
+    Context "Empty mappingfile" {
+        It "Does not replace anything" {
+            $script:dataFile = """Character Name,Account Handle,Resource Quantity,Resource
+                foo0,bar,1,stuff
+                f001,bar,2,more stuff
+            """
+            $script:mappingFile = @(
+                ""
+            )
+            $script:expectedResult = $script:dataFile
+
+            $convertArgs = @{
+                DonationLogPath = $donationFakePath
+                AltAccountMappingFile = $mappingFakePath
+                TargetFile = $targetFakePath
+            }
+            Convert-AltAccount @convertArgs | Should -BeNullOrEmpty
+
+            Should -InvokeVerifiable
+        }
+    }
+
+    Context "Accounts to replace" {
+        It "Single account" {
+            $script:dataFile = """Character Name,Account Handle,Resource Quantity,Resource
+                foo0,bar,1,stuff
+                f001,bar,2,more stuff
+                f002,replacer,3,even more stuff
+            """
+            $script:mappingFile = @(
+                "replacer:bar"
+            )
+            $script:expectedResult = """Character Name,Account Handle,Resource Quantity,Resource
+                foo0,bar,1,stuff
+                f001,bar,2,more stuff
+                f002,bar,3,even more stuff
+            """
+
+            $convertArgs = @{
+                DonationLogPath = $donationFakePath
+                AltAccountMappingFile = $mappingFakePath
+                TargetFile = $targetFakePath
+            }
+            Convert-AltAccount @convertArgs | Should -BeNullOrEmpty
+
+            Should -InvokeVerifiable
+        }
+
+        It "Single account, multiple entries" {
+            $script:dataFile = """Character Name,Account Handle,Resource Quantity,Resource
+                foo0,bar,1,stuff
+                f001,bar,2,more stuff
+                f002,replacer,3,even more stuff
+                f003,replacer,3,even more stuff
+                f002,replacer,3,even more stuff
+            """
+            $script:mappingFile = @(
+                "replacer:bar"
+            )
+            $script:expectedResult = """Character Name,Account Handle,Resource Quantity,Resource
+                foo0,bar,1,stuff
+                f001,bar,2,more stuff
+                f002,bar,3,even more stuff
+                f003,bar,3,even more stuff
+                f002,bar,3,even more stuff
+            """
+
+            $convertArgs = @{
+                DonationLogPath = $donationFakePath
+                AltAccountMappingFile = $mappingFakePath
+                TargetFile = $targetFakePath
+            }
+            Convert-AltAccount @convertArgs | Should -BeNullOrEmpty
+
+            Should -InvokeVerifiable
+        }
+
+        It "Multiple accounts" {
+            $script:dataFile = """Character Name,Account Handle,Resource Quantity,Resource
+                foo0,bar,1,stuff
+                f001,bar,2,more stuff
+                es1,barfoos,2,more stuff
+                f002,replacer0,3,even more stuff
+                f003,replacer0,3,even more stuff
+                es0,replacer1,3,even more stuff
+            """
+            $script:mappingFile = @(
+                "replacer0:bar"
+                "replacer1:barfoos"
+            )
+            $script:expectedResult = """Character Name,Account Handle,Resource Quantity,Resource
+                foo0,bar,1,stuff
+                f001,bar,2,more stuff
+                es1,barfoos,2,more stuff
+                f002,bar,3,even more stuff
+                f003,bar,3,even more stuff
+                es0,barfoos,3,even more stuff
+            """
+
+            $convertArgs = @{
+                DonationLogPath = $donationFakePath
+                AltAccountMappingFile = $mappingFakePath
+                TargetFile = $targetFakePath
+            }
+            Convert-AltAccount @convertArgs | Should -BeNullOrEmpty
+
+            Should -InvokeVerifiable
+        }
+    }
+}
---
+++ Convert-AltAccount.ps1
@@ -0,0 +1,51 @@
+<#
+.SYNOPSIS
+    A script that uses a mapping file to replace test in a textfile with a different text
+    and stores the result in a new file
+
+.PARAMETER DonationLogPath
+    The file with the donation data where people have multiple accounts
+
+.PARAMETER AltAccountMappingFile
+    The path to a file with mapping information
+    Each line in that file is an entry and separated by a colon ("altAccount#1234:MainAccount#1233")
+
+.PARAMETER TargetFile
+    The target file that should be created
+
+.EXAMPLE
+    .\Convert-AltAccount -DonationLogPath .\data\donations.csv -AltAccountMappingFile .\data\accountMapping.txt -TargetFile .\data\donationData_clean.csv
+#>
+
+
+
+function Convert-AltAccount {
+    param (
+        [Parameter(Mandatory)]
+        [String]
+        $DonationLogPath,
+        [Parameter(Mandatory)]
+        [String]
+        $AltAccountMappingFile,
+        [Parameter(Mandatory)]
+        [String]
+        $TargetFile
+    )
+
+    [array]$lines = Get-Content -Path $AltAccountMappingFile
+    [array]$accountsToMap = $lines | Where-Object {
+        -not [String]::IsNullOrWhiteSpace($_)
+    } | ForEach-Object {
+        $altAccount, $mainAccount = $_.Trim().Split(':')
+        @{
+            Alt = $altAccount
+            Main = $mainAccount
+        }
+    }
+
+    $fileContent = Get-Content -Path $DonationLogPath -Raw
+    $accountsToMap | ForEach-Object {
+        $fileContent = $fileContent.Replace($_.Alt, $_.Main)
+    }
+    New-Item -Path $TargetFile -ItemType File -Value $fileContent
+}
