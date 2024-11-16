ca43c9accf6a00be03eb3042120ff606f297b9ed
JohnnyDoesStuff <johnny.doesstuff@freenet.de>
2024-11-16 10:24:34 +0100

    Rankup: Add function to create a rankup proposal

--- PythonTools/Rankup/RankupProposal.py
+++ PythonTools/Rankup/RankupProposal.py
@@ -1,10 +1,16 @@
+from datetime import date, datetime
 import pandas as pd
 
 from PythonTools.Rankup.Account import Account
+from PythonTools.Rankup.RankupRule import RankupRule
 
 class RankupProposal:
     def __init__(self):
         print("Rankup init")
+
+    def convert_date_to_date_object(self, date_string: str) -> datetime:
+        # todo: support more timeformats
+        return datetime.strptime(date_string, "%d.%m.%Y, %H:%M:%S")
 
     def read_accounts(self, path: str) -> list:
         print(f"Reading accounts from {path}")
@@ -12,8 +18,29 @@ class RankupProposal:
 
         accounts = []
         for _, row in raw_account_data.iterrows():
+            join_date = self.convert_date_to_date_object(row['Join Date'])
             account = Account(
                 row['Account Handle'],
-                row['Join Date'])
+                row['Guild Rank'],
+                join_date
+            )
             accounts.append(account)
         return accounts
+
+    def create_rankup_proposal_for_accounts(self,
+                                            rankup_rule: RankupRule,
+                                            accounts: list,
+                                            reference_date: date) -> list:
+        rankup_proposal = []
+
+        for account in accounts:
+            join_date = account.join_date.date()
+            days_since_join = (reference_date - join_date).days
+
+            if (days_since_join >= rankup_rule.rankup_after) and \
+                (account.guild_rank == rankup_rule.rank):
+
+                rankup_proposal.append(account)
+
+        return rankup_proposal
+
