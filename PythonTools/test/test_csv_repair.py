c094a8e415101810c24241eb3c896fc457be4d80
JohnnyDoesStuff <johnny.doesstuff@freenet.de>
2024-11-24 09:02:00 +0100

    Rankup: Repair more csv errors
    - more dates
    - date as last value
    - also make testdata more consistent

--- PythonTools/test/test_csv_repair.py
+++ PythonTools/test/test_csv_repair.py
@@ -29,3 +29,42 @@ class TestRepairCsv(unittest.TestCase):
             self.assertEqual(fixed_data['Account Handle'][1], '@baz')
             self.assertEqual(fixed_data['Guild Rank'][1], 'FakeRank2')
             self.assertEqual(fixed_data['Join Date'][1], '2.1.2024, 10:00:00')
+
+    def test_repair_also_fixes_other_dates(self):
+        file_path = "testdata/accountsGermanExport.csv"
+        current_directory = os.path.dirname(
+            os.path.abspath(__file__)
+        )
+        source_path = os.path.join(current_directory, file_path)
+        repair_tool = RepairCsv()
+
+        with tempfile.TemporaryDirectory() as temp_dir:
+            fix_file_path = os.path.join(temp_dir, 'fixes.csv')
+            repair_tool.repair_csv(source_path, fix_file_path)
+
+            fixed_data = pd.read_csv(fix_file_path)
+
+            self.assertEqual(fixed_data['Rank Change Date'][0], '2.1.2024, 11:00:00')
+            self.assertEqual(fixed_data['Last Active Date'][0], '3.1.2024, 12:00:00')
+            self.assertEqual(fixed_data['Rank Change Date'][1], '2.1.2024, 11:00:00')
+            self.assertEqual(fixed_data['Last Active Date'][1], '3.1.2024, 12:00:00')
+
+    def test_repair_fixes_date_as_last_value(self):
+        file_path = "testdata/accountsDateAsLastValue.csv"
+        current_directory = os.path.dirname(
+            os.path.abspath(__file__)
+        )
+        source_path = os.path.join(current_directory, file_path)
+        repair_tool = RepairCsv()
+
+        with tempfile.TemporaryDirectory() as temp_dir:
+            fix_file_path = os.path.join(temp_dir, 'fixes.csv')
+            repair_tool.repair_csv(source_path, fix_file_path)
+
+            fixed_data = pd.read_csv(fix_file_path)
+
+            self.assertEqual(fixed_data['Rank Change Date'][0], '2.1.2024, 11:00:00')
+            self.assertEqual(fixed_data['Last Active Date'][0], '3.1.2024, 12:00:00')
+            self.assertEqual(fixed_data['Public Comment Last Edit Date'][0], '2.1.2024, 11:30:00')
+            self.assertEqual(fixed_data['Rank Change Date'][1], '2.1.2024, 11:00:00')
+            self.assertEqual(fixed_data['Last Active Date'][1], '3.1.2024, 12:00:00')
