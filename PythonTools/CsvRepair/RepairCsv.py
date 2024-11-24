4ad4bb446d287dee072012102bd96165269e3b05
JohnnyDoesStuff <johnny.doesstuff@freenet.de>
2024-11-24 09:02:00 +0100

    Rankup: Repair more csv errors
    - more dates
    - date as last value
    - also make testdata more consistent

--- PythonTools/CsvRepair/RepairCsv.py
+++ PythonTools/CsvRepair/RepairCsv.py
@@ -11,14 +11,29 @@ class RepairCsv:
 
         # matches this kind of format: 1.2.2024, 12:34:56
         find_date = r',(\d{1,2}\.\d{1,2}\.\d{4}), (\d{1,2}:\d{1,2}:\d{1,2}),'
+        find_date_at_end = r',(\d{1,2}\.\d{1,2}\.\d{4}), (\d{1,2}:\d{1,2}:\d{1,2})$'
         date_with_quotation_marks = r',"\1, \2",'
 
         for line_to_fix in text_lines:
-            fixed_line = re.sub(
+            semi_fixed_line = re.sub(
                 find_date,
                 date_with_quotation_marks,
                 line_to_fix
             )
+            # applying it again because there are multiple dates
+            # right after each other
+            fixed_line = re.sub(
+                find_date,
+                date_with_quotation_marks,
+                semi_fixed_line
+            )
+
+            fixed_line = re.sub(
+                find_date_at_end,
+                date_with_quotation_marks,
+                fixed_line
+            )
+
             fixed_lines.append(fixed_line)
 
         with open(target_path, 'w') as f:
