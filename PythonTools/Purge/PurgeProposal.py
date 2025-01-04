3722499a161c77f7615082997d0a9dee8e422432
JohnnyDoesStuff <johnny.doesstuff@freenet.de>
2025-01-04 09:45:24 +0100

    Purge: Document functions of PurgeProposal

--- PythonTools/Purge/PurgeProposal.py
+++ PythonTools/Purge/PurgeProposal.py
@@ -8,6 +8,15 @@ class PurgeProposal:
         print("Purge init")
 
     def read_rules(self, path: str):
+        """
+        Reads rule information from a CSV file and returns a list of PurgeRule objects.
+
+        Args:
+            path (str): The path to the CSV file containing the purge rules.
+
+        Returns:
+            list: A list of PurgeRule objects.
+        """
         print(f"Reading rules from {path}")
         raw_rule_data = pd.read_csv(path)
 
@@ -23,8 +32,23 @@ class PurgeProposal:
     def create_purge_proposal(self,
                         rule: PurgeRule,
                         accounts: list,
-                        reference_date: date
+                        reference_date: date = date.today()
                         ) -> list:
+        """
+        Creates a purge proposal for the given accounts based on the given purge rule
+        and reference date.
+
+        Args:
+            rule (PurgeRule): The purge rule to use for the purge proposal.
+            accounts (list): A list of Account objects.
+            reference_date (date): The reference date to use to calculate the time
+                when the purge should be done.
+                Usually that's the value of date.today()
+
+        Returns:
+            list: A list of Account objects that should receive a purge
+                according to the given rule.
+        """
         purge_proposal = []
         max_inactivity_time = timedelta(days=rule.purge_after)
 
